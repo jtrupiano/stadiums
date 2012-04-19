c13c1f8b3f0bab65ab364d3c1e186255b769598d
John Trupiano <jtrupiano@gmail.com>
2012-04-19 10:31:20 -0500

    restore import_data script full first-time use

--- import_data.rb
+++ import_data.rb
@@ -1,6 +1,6 @@
 require './environment'
 importer = DataImporter.new
 # importer.clear_data
-# importer.import_stadiums
+importer.import_stadiums
 importer.import_distances(false)
-# importer.import_schedule
+importer.import_schedule
